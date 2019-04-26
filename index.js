afc0287781120a22122f1f3ac73fb0b720e21193
Alenkart Rodriguez <alenkart.r@gmail.com>
2019-04-25 21:25:30 -0400

    Created the auth route and creared a secure method

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 //index.js
 'use strict';
 
+require('dotenv').config();
+
 const express = require('express');
-const router = express.Router();
 const helmet = require('helmet');
 const validator = require('express-validator/check');
+const passport = require('./lib/utils/passport.util');
 
-require('dotenv').config();
-require('./lib/models');
+require('./lib/utils/mongoose.util');
+
 const app = express();
 
 //configs
 app.use(helmet());
 app.use(express.json());
+app.use(passport.initialize());
 
 //constrollers
-const controllersArgs = { router, validator };
+const controllersArgs = { validator, passport };
 app.use('/user', require('./lib/controllers/user.controller')(controllersArgs));
+app.use('/auth', require('./lib/controllers/auth.controller')(controllersArgs));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running at port: ${process.env.PORT}`);
