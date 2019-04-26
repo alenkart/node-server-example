37e825152666658d59ce3c5f7b28f934f6a350bd
Alenkart Rodriguez <alenkart.r@gmail.com>
2019-04-25 22:47:28 -0400

    Created the winstom util and the stream method to log the morgan requests

--- index.js
+++ index.js
@@ -5,19 +5,28 @@ const express = require('express');
 const router = express.Router();
 const helmet = require('helmet');
 const validator = require('express-validator/check');
+const logger = require('./lib/utils/winstom.util');
+const morgan = require('./lib/utils/morgan.util')
 
 require('dotenv').config();
 require('./lib/models');
+
 const app = express();
 
 //configs
 app.use(helmet());
+app.use(morgan('combined', logger.stream));
 app.use(express.json());
 
+
 //constrollers
-const controllersArgs = { router, validator };
+const controllersArgs = {
+    logger,
+    router,
+    validator
+};
 app.use('/user', require('./lib/controllers/user.controller')(controllersArgs));
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running at port: ${process.env.PORT}`);
+    logger.info(`Server is running at port: ${process.env.PORT}, environment: ${process.env.NODE_ENV}`);
 });
